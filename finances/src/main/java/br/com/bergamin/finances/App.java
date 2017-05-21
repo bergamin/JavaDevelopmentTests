04fab4f71b579b04c7dc965389f09bc63fdb70d2
bergamin <taffarel@outlook.com>
2017-05-21 15:42:19 -0300

    Finances project re-added

---
+++ finances/src/main/java/br/com/bergamin/finances/App.java
@@ -0,0 +1,13 @@
+package br.com.bergamin.finances;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
