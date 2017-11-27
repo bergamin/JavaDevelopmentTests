7ac8528ede30c388cf42d5b046758b0ce843ccf6
Guilherme Taffarel Bergamin <taffarel@outlook.com>
2017-11-26 23:13:26 -0200

    - Sending all changes to repository as first step before reorganizing repository

--- urnaeletronica/src/main/java/br/com/bergamin/urnaeletronica/util/JPAUtil.java
+++ urnaeletronica/src/main/java/br/com/bergamin/urnaeletronica/util/JPAUtil.java
@@ -6,7 +6,7 @@ import javax.persistence.Persistence;
 
 public class JPAUtil {
 
-	private static EntityManagerFactory EMF = Persistence.createEntityManagerFactory("finances-mysql");
+	private static EntityManagerFactory EMF = Persistence.createEntityManagerFactory("urnaeletronica-mysql");
 	
 	public EntityManager getEntityManager(){
 		return EMF.createEntityManager();
